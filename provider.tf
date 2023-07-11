739747c0e360fbf63d0d512abebbf98c7098844e
Douglass Coronado <dcoronado@funio.com>
2023-07-11 09:58:16 -0400

    Add Google provider and terraform workspace

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 4.57.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+}
+
+terraform {
+  cloud {
+    organization = "dcoronado-mcit"
+    workspaces {
+      name = "mcti-test"
+    }
+  }
+}

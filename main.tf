567516f8943aa9d794b01554f942ddb8a71c5c2b
Douglass Coronado <dcoronado@funio.com>
2023-08-10 23:19:54 -0400

    Allow allusers to invoke the cloud functions

--- main.tf
+++ main.tf
@@ -142,6 +142,16 @@ resource "google_cloudfunctions_function" "my_function" {
    ] 
 }
 
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.my_function.project
+  region         = google_cloudfunctions_function.my_function.region
+  cloud_function = google_cloudfunctions_function.my_function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
 resource "google_storage_bucket" "my_bucket" {
   name     = "my-function-bucket-${var.project_id}"
   location = "US"
